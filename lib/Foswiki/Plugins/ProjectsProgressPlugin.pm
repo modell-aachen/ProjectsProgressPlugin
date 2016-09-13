89949c992c7cb9d5c062e465c943cf159a331a96
Sven Meyer <meyer@modell-aachen.de>
2016-09-13 19:20:01 +0200

    First working draft
    Ticket: #14070

--- lib/Foswiki/Plugins/ProjectsProgressPlugin.pm
+++ lib/Foswiki/Plugins/ProjectsProgressPlugin.pm
@@ -6,6 +6,8 @@ use warnings;
 
 use Foswiki::Func;
 use Foswiki::Plugins;
+use Foswiki::Plugins::JQueryPlugin;
+use JSON;
 
 our $VERSION = '1.0.0';
 our $RELEASE = '1.0.0';
@@ -13,21 +15,171 @@ our $SHORTDESCRIPTION = 'Renders progress information for project apps/Plugins';
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub initPlugin {
-  my ( $topic, $web, $user, $installWeb ) = @_;
-  # check for Plugins.pm versions
-  if ( $Foswiki::Plugins::VERSION < 2.0 ) {
+  my ($topic, $web, $user, $installWeb) = @_;
+  if ($Foswiki::Plugins::VERSION < 2.0) {
     Foswiki::Func::writeWarning( 'Version mismatch between ',
     __PACKAGE__, ' and Plugins.pm' );
     return 0;
   }
 
-  Foswiki::Func::registerTagHandler('MILESTONEINFO', \&_tagMILESTONEINFO);
-
+  Foswiki::Func::registerTagHandler('MILESTONEINFO', \&tagMILESTONEINFO);
   return 1;
 }
 
 sub tagMILESTONEINFO {
   my($session, $params, $topic, $web, $topicObject) = @_;
+
+  my $project = $params->{_DEFAULT} || $params->{project} || '';
+  return '%RED%<span>Missing param "project"!</span>%ENDCOLOR%' unless $project;
+
+  my $type = $params->{type} || 'timeline';
+  return '%RED%<span>Invalid type specified!</span>%ENDCOLOR%' unless $type =~ /^(timeline|milestone|next)$/;
+
+  my $milestones = $params->{milestones} || '';
+  $milestones =~ s/\s+//g;
+
+  my @milestones = split(',', $milestones);
+  return '%RED%<span>Missing param "milestones"!</span>%ENDCOLOR%' unless scalar(@milestones);
+
+  _injectDeps();
+  my $doneField = $params->{donefield} || '$msDone';
+  my $dueField = $params->{duefield} || '$msDueDate';
+
+  my ($pweb, $ptopic) = Foswiki::Func::normalizeWebTopicName(undef, $project);
+  if ($type eq 'milestone') {
+    my $milestone = $params->{milestone} || '';
+    return '%RED%<span>Missing param "milestone"!</span>%ENDCOLOR%' unless $milestones;
+
+    my $ms = _readMilestone($pweb, $ptopic, [$milestone], $doneField, $dueField);
+    return _toHTML('milestone', shift(@{$ms}));
+  }
+
+  my @retval;
+  my $returnNext = 0;
+  @milestones = @{_readMilestone($pweb, $ptopic, \@milestones, $doneField, $dueField)};
+
+  foreach my $ms (@milestones) {
+    if ($type eq 'next') {
+      my $isDone = $ms->{done} eq JSON::true;
+      my $returnHere = $returnNext && !$isDone;
+
+      if ($returnHere) {
+        return _toHTML('milestone', $ms);
+      } else {
+        my ($index)= grep {@milestones[$_] == $ms} 0..$#milestones;
+        return '' if ($index + 1 == scalar(@milestones));
+      }
+
+      $returnNext = $isDone;
+    }
+
+    push(@retval, $ms);
+  }
+
+  return _toHTML('milestone', shift(@retval)) if $type eq 'next';
+  return _toHTML('timeline', \@retval);
+}
+
+sub _readMilestone {
+  my ($web, $topic, $milestones, $doneField, $dueField) = @_;
+
+  my @retval;
+  my ($meta, $text) = Foswiki::Func::readTopic($web, $topic);
+
+  foreach my $milestone (@$milestones) {
+    my $titlePref = "PROJECT_${milestone}_TITLE";
+    my $done = $doneField;
+    my $due = $dueField;
+    $done =~ s/\$ms/$milestone/;
+    $due =~ s/\$ms/$milestone/;
+
+    my $title = Foswiki::Func::getPreferencesValue($titlePref, $web);
+    $title = $meta->expandMacros($title);
+    my $due = $meta->get('FIELD', $due);
+    my $done = $meta->get('FIELD', $done);
+
+    push @retval, {
+      title => "$title",
+      due => int($due->{value}),
+      done => ($done->{value} eq 'done') ? JSON::true : JSON::false,
+      milestone => $milestone
+    };
+  }
+
+  $meta->finish();
+  return \@retval;
+}
+
+sub _toHTML {
+  my ($type, $data, $foo) = @_;
+  my $json = to_json($data);
+
+  if ($type eq 'milestone') {
+    my $date = Foswiki::Time::formatTime($data->{due}, '$day.$mo.$ye');
+    my $state = ($data->{done} eq JSON::true) ? 'closed' : 'open';
+    return <<HTML;
+<div class="milestone">
+  <div class="signal">%SIGNAL{"$data->{due}" status="$state"}%</div>
+  <div class="text">
+    <span><strong>$date</strong></span>
+    <span>$data->{title}</span>
+    <span class="rawdata">$json</span>
+  </div>
+</div>
+HTML
+  }
+
+  my @entries;
+  my $nextIsActive = 0;
+  foreach my $entry (@$data) {
+    my $icon = 'fa-play' if $nextIsActive;
+    $nextIsActive = $entry->{done} eq JSON::true;
+    $icon = 'fa-check' if $nextIsActive;
+    $icon = '' unless defined $icon;
+
+    my $warn = 3 * 24 * 60 * 60;
+    my $now = time;
+    my $color = 'green';
+    if ($entry->{due} < $now) {
+      $color = 'red';
+    } elsif ($now + $warn >= $entry->{due}) {
+      $color = 'yellow';
+    }
+
+    $color = '' if $entry->{done} eq JSON::true;
+    my $fa = "<i class=\"fa $icon\"></i>";
+    my $html = <<HTML;
+<div class="entry $color">
+  <span class="bar"></span>
+  <span class="circle">$fa</span>
+</div>
+HTML
+    push @entries, $html;
+  }
+
+  my $inner = join('', @entries);
+  return <<HTML;
+  <div class="timeline">$inner</div>
+HTML
+}
+
+sub _injectDeps {
+  Foswiki::Plugins::JQueryPlugin::createPlugin('jqp::moment');
+  Foswiki::Func::addToZone('script', 'VUEJSPLUGIN', "<script type=\"text/javascript\" src=\"%PUBURLPATH%/%SYSTEMWEB%/VueJSPlugin/vue.min.js\"></script>");
+  Foswiki::Func::addToZone('head', "VUEJS::STYLES", "<link rel=\"stylesheet\" type=\"text/css\" href=\"%PUBURLPATH%/%SYSTEMWEB%/VueJSPlugin/vue.css\" />");
+
+  my $styles = <<STYLES;
+<link rel="stylesheet" type="text/css" media="all" href="%PUBURLPATH%/%SYSTEMWEB%/FontAwesomeContrib/css/font-awesome.min.css%QUERYVERSION{name="FontAwesomeContrib" format="?version=\$version"}%" />
+<link rel="stylesheet" type="text/css" media="all" href="%PUBURLPATH%/%SYSTEMWEB%/ProjectsProgressPlugin/css/progress.css?version=$RELEASE" />
+STYLES
+  Foswiki::Func::addToZone('head', 'PROJECTSPROGRESS::CSS', $styles);
+
+  Foswiki::Func::addToZone(
+    'script',
+    'PROJECTSPROGRESS::JS',
+    "<script type=\"text/javascript\" src=\"%PUBURLPATH%/%SYSTEMWEB%/ProjectsProgressPlugin/js/progress.js\"></script>",
+    'JQUERYPLUGIN::JQP::MOMENT,VUEJSPLUGIN,JQUERYPLUGIN::FOSWIKI::PREFERENCES'
+  );
 }
 
 1;
@@ -35,7 +187,7 @@ sub tagMILESTONEINFO {
 __END__
 Q.Wiki ProjectsProgressPlugin - Modell Aachen GmbH
 
-Author: %%
+Author: %$AUTHOR%
 
 Copyright (C) 2016 Modell Aachen GmbH
 
---
+++ pub/System/ProjectsProgressPlugin/js/progress.js
@@ -0,0 +1,4 @@
+;(function ($, document, window, undefined) {
+  $(document).ready(function() {
+  });
+}(jQuery, document, window, undefined));
